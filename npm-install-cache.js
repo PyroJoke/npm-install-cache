38cdfb5ec669d41bb2149db57158ad4ca8a62fd0
Dmytro Mykhailov <dmytro_mykhailov@epam.com>
2015-08-10 16:26:15 +0300

    Stable 3

--- npm-install-cache.js
+++ npm-install-cache.js
@@ -13,62 +13,68 @@ function checksum (str, algorithm, encoding) {
         .digest(encoding || 'hex')
 }
 
-fs.readFile('package.json', function (err, data) {
-    var fileChecksum = checksum(data);
-    console.log('Calculated file checksum ' + fileChecksum);
-
-    getCachedModules(fileChecksum, createSymbolicLinkToNodeModules, notFoundCallback);
-
-    function getCachedModules(checksum, found, notFound) {
-        searchForCachedVersion(checksum, function(searchFoundSomething, searchDir, folderContent) {
-            if(searchFoundSomething) {
-                found(searchDir, folderContent);
-            } else {
-                notFound(searchDir);
-            }
-        });
-    }
-      
-    function searchForCachedVersion(checksum, cb) {
-        var searchDir = options.cacheDir + checksum;
-        fs.readdir(searchDir, function(err, folderContent) {
-            if(err) {
-                console.log("Directory " + searchDir + " was not found");
-                cb(false, searchDir);
-                return;
-            }
-
-            cb(true, searchDir, folderContent);
-            return;
-        });
-    }
+function useCacheFolder(fileToCheck, localFolderToCache, getPackagesCommand) {
+    fs.readFile(fileToCheck, function (err, data) {
+        var fileChecksum = checksum(data);
+        console.log('Calculated file checksum ' + fileChecksum);
     
-    function notFoundCallback(searchDir) {
-        console.log('Running npm install');
-        //Run npm install
-        var npm_install = spawn('powershell', ['npm install']);
-        npm_install.stdout.on('data', function(text) {console.log(text.toString())});
-        npm_install.stdout.on('end', function() {
-            console.log('Done installing NPM modules');
-
-            console.log('Moving to ' +  searchDir);
-            var move = spawn('powershell', ['mv node_modules', searchDir]);
-            move.stdout.on('end', function() {
-                console.log('Done moving');
-                
-                createSymbolicLinkToNodeModules(searchDir);
+        getCachedModules(fileChecksum, createSymbolicLinkToNodeModules, notFoundCallback);
+    
+        function getCachedModules(checksum, found, notFound) {
+            searchForCachedVersion(checksum, function(searchFoundSomething, searchDir, folderContent) {
+                if(searchFoundSomething) {
+                    found(searchDir, folderContent);
+                } else {
+                    notFound(searchDir);
+                }
             });
-        });
-
-    }
+        }
+          
+        function searchForCachedVersion(checksum, cb) {
+            var searchDir = options.cacheDir + checksum;
+            fs.readdir(searchDir, function(err, folderContent) {
+                if(err) {
+                    console.log("Directory " + searchDir + " was not found");
+                    cb(false, searchDir);
+                    return;
+                }
     
-    function createSymbolicLinkToNodeModules(searchDir, folderContent) {
-        console.log('Creating symbolic link to node modules: ' + searchDir);
+                cb(true, searchDir, folderContent);
+                return;
+            });
+        }
         
-        var destPath = __dirname + '\\node_modules';
-        console.log('Destination path: '+destPath);
-        fs.symlink(searchDir, destPath, 'dir', function() {
-            console.log('Symlink created');
-        });
-    }
-});
+        function notFoundCallback(searchDir) {
+            console.log('Running npm install');
+            //Run npm install
+            var npm_install = spawn('powershell', [getPackagesCommand]);
+            npm_install.stdout.on('data', function(text) {console.log(text.toString())});
+            npm_install.stdout.on('end', function() {
+                console.log('Done installing NPM modules');
+    
+                console.log('Moving to ' +  searchDir);
+                var move = spawn('powershell', ['mv ' + localFolderToCache, searchDir]);
+                move.stdout.on('end', function() {
+                    console.log('Done moving');
+                    
+                    createSymbolicLinkToNodeModules(searchDir);
+                });
+            });
+    
+        }
+        
+        function createSymbolicLinkToNodeModules(searchDir, folderContent) {
+            console.log('Creating symbolic link to node modules: ' + searchDir);
+            
+            var destPath = __dirname + '\\' + localFolderToCache;
+            console.log('Destination path: '+destPath);
+            fs.symlink(searchDir, destPath, 'dir', function() {
+                console.log('Symlink created');
+            });
+        }
+    });
+
+}
+
+
+useCacheFolder('package.json', 'node_modules', 'npm install');
