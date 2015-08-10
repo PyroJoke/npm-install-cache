bfd3a1c4fa9cfe703192cf0319bd786dba6aa37f
Dmytro Mykhailov <dmytro_mykhailov@epam.com>
2015-08-10 15:46:52 +0300

    Stable 1

--- npm-install-cache.js
+++ npm-install-cache.js
@@ -1,6 +1,10 @@
 var crypto = require('crypto'), 
     fs = require('fs'),
     spawn = require('child_process').spawn;
+    
+var options = {
+    cacheDir: 'C:\\temp\\npm-cache\\'
+};
 
 function checksum (str, algorithm, encoding) {
     return crypto
@@ -26,7 +30,7 @@ fs.readFile('package.json', function (err, data) {
     }
       
     function searchForCachedVersion(checksum, cb) {
-        var searchDir = "/tmp/npmCache/" + checksum;
+        var searchDir = options.cacheDir + checksum;
         fs.readdir(searchDir, function(err, folderContent) {
             if(err) {
                 console.log("Directory " + searchDir + " was not found");
@@ -38,25 +42,33 @@ fs.readFile('package.json', function (err, data) {
             return;
         });
     }
-
-    function foundCallback(searchDir, folderContent) {
-        console.log('Found the dir, copy make link to current folder instead of run npm install');
-    }
     
     function notFoundCallback(searchDir) {
         console.log('Not found the dir, run npm install');
         //Run npm install
-        var npm_install = spawn('npm', ['install']);
+        var npm_install = spawn('powershell', ['npm install']);
         npm_install.stdout.on('data', function(text) {console.log(text.toString())});
         npm_install.stdout.on('end', function() {
             console.log('Done installing NPM modules');
 
             console.log('Moving to ' +  searchDir);
-            var move = spawn('mv', ['node_modules', searchDir]);
+            var move = spawn('powershell', ['mv node_modules', searchDir]);
             move.stdout.on('end', function() {
                 console.log('done moving');
+                
+                
             });
         });
 
+    }
+    
+    function foundCallback(searchDir, folderContent) {
+        console.log('Found the dir, copy make link to current folder instead of run npm install: ' + folderContent);
+        
+        var create_symlink = spawn('cmd', ['/C "mklink /D /J node_modules ' + searchDir + '"']);
+        create_symlink.stdout.on('data', function(text) {console.log(text.toString())});
+        create_symlink.stdout.on('end', function() {
+            console.log('done creating symbolic link to ' + searchDir);
+        });
     }
 });
