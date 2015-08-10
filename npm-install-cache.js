ca6897e98f8a7a82d84c10a696b1e11cae96ef85
Dmytro Mykhailov <dmytro_mykhailov@epam.com>
2015-08-10 16:08:52 +0300

    Stable 2

--- npm-install-cache.js
+++ npm-install-cache.js
@@ -17,7 +17,7 @@ fs.readFile('package.json', function (err, data) {
     var fileChecksum = checksum(data);
     console.log('Calculated file checksum ' + fileChecksum);
 
-    getCachedModules(fileChecksum, foundCallback, notFoundCallback);
+    getCachedModules(fileChecksum, createSymbolicLinkToNodeModules, notFoundCallback);
 
     function getCachedModules(checksum, found, notFound) {
         searchForCachedVersion(checksum, function(searchFoundSomething, searchDir, folderContent) {
@@ -44,7 +44,7 @@ fs.readFile('package.json', function (err, data) {
     }
     
     function notFoundCallback(searchDir) {
-        console.log('Not found the dir, run npm install');
+        console.log('Running npm install');
         //Run npm install
         var npm_install = spawn('powershell', ['npm install']);
         npm_install.stdout.on('data', function(text) {console.log(text.toString())});
@@ -54,21 +54,29 @@ fs.readFile('package.json', function (err, data) {
             console.log('Moving to ' +  searchDir);
             var move = spawn('powershell', ['mv node_modules', searchDir]);
             move.stdout.on('end', function() {
-                console.log('done moving');
+                console.log('Done moving');
                 
-                
+                createSymbolicLinkToNodeModules(searchDir);
             });
         });
 
     }
     
-    function foundCallback(searchDir, folderContent) {
-        console.log('Found the dir, copy make link to current folder instead of run npm install: ' + folderContent);
+    function createSymbolicLinkToNodeModules(searchDir, folderContent) {
+        console.log('Creating symbolic link to node modules: ' + searchDir);
         
-        var create_symlink = spawn('cmd', ['/C "mklink /D /J node_modules ' + searchDir + '"']);
-        create_symlink.stdout.on('data', function(text) {console.log(text.toString())});
-        create_symlink.stdout.on('end', function() {
-            console.log('done creating symbolic link to ' + searchDir);
+        var destPath = __dirname + '\\node_modules';
+        console.log('Destination path: '+destPath);
+        fs.symlink(searchDir, destPath, 'junction', function() {
+            console.log('Symlink created');
         });
+        
+        // var params = '/C "mklink /D /J node_modules ' + searchDir + '"';
+        // console.log("Make link params: " + params);
+        // var create_symlink = spawn('cmd', [params]);
+        // create_symlink.stdout.on('data', function(text) {console.log(text.toString())});
+        // create_symlink.stdout.on('end', function() {
+        //     console.log('Done creating symbolic link to ' + searchDir);
+        // });
     }
 });
